3326d90a315c2129d592213d1b789dd9b58e3e25
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-08-19 22:50:49 +0600

    Added Doctors and oracle database connection

---
+++ Doctors/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import cx_Oracle
+from django.http import HttpResponse
+
+
+def doctors(request):
+    dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='ORCL')
+    conn = cx_Oracle.connect(user='MEDI_SHEBA', password='1234', dsn=dsn_tns)
+    c = conn.cursor()
+    print(c)
+    print('Success')
+    c.execute("SELECT * from MEDI_SHEBA.DOCTORS")
+    out = ''
+    for row in c:
+        out += str(row) + ' \n '
+    conn.close()
+    return HttpResponse(out, content_type="text/plain")
