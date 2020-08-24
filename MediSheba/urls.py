d6ed9b5197638fb00ddef20c7eb4fdc6be71e0d0
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-08-24 21:01:07 +0600

    Added User app. Users can view Doctors list

--- MediSheba/urls.py
+++ MediSheba/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("LogIn.urls")),
+    path('', include("Users.urls")),
+
 ]
---
+++ Users/views.py
@@ -0,0 +1,19 @@
+import cx_Oracle
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def seeDoc(request):
+    dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='ORCL')
+    conn = cx_Oracle.connect(user='MEDI_SHEBA', password='1234', dsn=dsn_tns)
+    c = conn.cursor()
+    print(c)
+    print('Success')
+    c.execute("SELECT * from MEDI_SHEBA.DOCTORS")
+    out = ''
+    for row in c:
+        out += str(row) + ' \n '
+    conn.close()
+    return HttpResponse(out, content_type="text/plain")
