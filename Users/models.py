1fbee85143372c795f53aaecf0452db19f50f6fa
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-08-25 11:55:37 +0600

    Displaying doctors name in loop

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class DoctorName:
+
+    def __init__(self, id, first_name, last_name, phone):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
--- Users/views.py
+++ Users/views.py
@@ -1,11 +1,14 @@
 import cx_Oracle
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import DoctorName
 
 
 # Create your views here.
 
 def seeDoc(request):
+    docList = []
+
     dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='ORCL')
     conn = cx_Oracle.connect(user='MEDI_SHEBA', password='1234', dsn=dsn_tns)
     c = conn.cursor()
@@ -14,6 +17,11 @@ def seeDoc(request):
     c.execute("SELECT * from MEDI_SHEBA.DOCTORS")
     out = ''
     for row in c:
-        out += str(row) + ' \n '
+        docList.append(DoctorName(row[0], row[1], row[2], row[3]))
     conn.close()
-    return HttpResponse(out, content_type="text/plain")
+
+    for obj in docList:
+        print(obj.id)
+        print(obj.first_name)
+
+    return render(request, "DoctorName.html", {'doc': docList})
