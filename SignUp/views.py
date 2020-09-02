438bc258a39404c0d5bf5988a032fe1dc097da07
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-09-02 14:31:11 +0600

    Renamed DOCTOR_LOGIN_DB.sql to DOCTOR_AUTHENTICATION.sql and created doctors password saving system

--- SignUp/views.py
+++ SignUp/views.py
@@ -19,15 +19,29 @@ def signupSubmit(request):
         dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='ORCL')
         conn = cx_Oracle.connect(user='MEDI_SHEBA', password='1234', dsn=dsn_tns)
         c = conn.cursor()
-        num = random.randrange(0, 1000, 2)
+        c1 = conn.cursor()
+        c2 = conn.cursor()
+
         statement = "INSERT INTO MEDI_SHEBA.DOCTORS(FIRST_NAME, LAST_NAME, EMAIL, PHONE) VALUES (" + "\'" + firstname + \
                     "\', " + "\'" + lastname + "\'," + "\'" + email + "\', " + "\'" + phone + "\'" + ")"
         c.execute(statement)
         conn.commit()
+        # print("SUCCESS INSERTING INTO DOCTORS")
 
-        print(statement)
+        statement2 = "SELECT DOCTOR_ID FROM MEDI_SHEBA.DOCTORS WHERE EMAIL =" + "\'" + email + "\'"
 
-        print("SUCCESS INSERTING INTO DOCTORS")
+        c1.execute(statement2)
+
+        temp = 0
+        for x in c1:
+            temp = x
+        print(temp[0])
+
+        statement3 = "INSERT INTO MEDI_SHEBA.DOCTOR_AUTHENTICATION(DOCTOR_ID, EMAIL, PASSWORD) VALUES ("+ str(temp[0]) + "," \
+                     + "\'" + email + "\'," + "\'" + password + "\'" + ")"
+        c2.execute(statement3)
+        conn.commit()
+
         return render(request, "DoctorHome.html")
 
     elif usertype == 'user':
