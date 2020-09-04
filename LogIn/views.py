c2170534cc513cdbc32f60e2bd55bdd718689e20
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-09-04 19:13:12 +0600

    Log In logic added

--- LogIn/views.py
+++ LogIn/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import cx_Oracle
 
 
 # Create your views here.
@@ -17,14 +18,29 @@ def submit(request):
     print("PASS: " + password)
     print("User Type: " + user)
 
+    dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='ORCL')
+    conn = cx_Oracle.connect(user='MEDI_SHEBA', password='1234', dsn=dsn_tns)
+    c = conn.cursor()
+
     # TODO: connect database and verify
     if user == "Doctor":
-        return render(request, "DoctorHome.html", {'user': email})
+        statement = "SELECT DOCTOR_ID from MEDI_SHEBA.DOCTOR_AUTHENTICATION WHERE EMAIL=" + "\'" + email + "\'" + \
+                    "AND PASSWORD =" + "\'"+password + "\' "
+        c.execute(statement)
+        if c:
+            for x in c:
+                print(x)
+                return render(request, "DoctorHome.html", {'user': email})
+        else:
+            print("NOT FOUND")
+
     elif user == "User":
         return render(request, "UserHome.html")
     elif user == "MediShopAdmin":
         return render(request, "PharmacyManagerHome.html")
-    return render(request, "HospitalAdminHome.html")
+    elif user=="HospitalAdmin":
+        return render(request, "HospitalAdminHome.html")
+    return render(request, "LogInOrSignUp.html")
 
 
 def signup(request):
