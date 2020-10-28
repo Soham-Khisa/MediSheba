3480c0e565a2882699b1d288afa7ac868b576b86
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-10-28 19:09:12 +0600

    Password encryption and decryption added

--- HelperClasses/encryptPass.py
+++ HelperClasses/encryptPass.py
@@ -13,3 +13,19 @@ class EncryptPasswords:
         encrypted = cipher_suit.encrypt(b_string)
         str_encrypted = encrypted.decode('utf-8')
         return str_encrypted
+
+    def decryptPassword(self):
+        b_string = str.encode(self.string)
+        key = b'5Zn0_tUZxQows6fwBur2GCo3q_uNm3Q8VuQBHx22BW0='
+        cipher_suit = Fernet(key)
+        decoded = cipher_suit.decrypt(b_string)
+        str_decoded = decoded.decode('utf-8')
+        return str_decoded
+
+'''
+en = EncryptPasswords("GGWP").encryptPassword()
+print(en)
+
+dec = EncryptPasswords(en).decryptPassword()
+print(dec)
+'''
