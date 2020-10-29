a1230104ded1e17cfe11c81dee6dfe33f1f637fd
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-10-29 20:23:10 +0600

    Authentication added during Log In

--- LogIn/views.py
+++ LogIn/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import cx_Oracle
 
+import HelperClasses.encryptPass as decoder
+
 
 # Create your views here.
 
@@ -26,22 +28,27 @@ def submit(request):
     # dsn_tns = cx_Oracle.makedsn('localhost', '1521', service_name='ORCL')
     # conn = cx_Oracle.connect(user='MEDI_SHEBA', password='1234', dsn=dsn_tns)
 
-    #------#
-
+    # ------#
 
     c = conn.cursor()
 
     # TODO: connect database and verify
     if user == "Doctor":
-        statement = "SELECT DOCTOR_ID from MEDI_SHEBA.DOCTOR WHERE EMAIL=" + "\'" + email + "\'" + \
-                    "AND PASSWORD =" + "\'" + password + "\' "
+        statement = "SELECT DOCTOR_ID, PASSWORD from MEDI_SHEBA.DOCTOR WHERE EMAIL=" + "\'" + email + "\'"
         c.execute(statement)
         if c:
-            temp = 0
-            for x in c:
-                temp = x
-                print("DOCTOR ID:" + str(temp[0]))
-                return render(request, "homepage/DoctorHome.html", {'user': email})
+            x = c.fetchone()
+            return_id = x[0]
+            return_password = x[1]
+            # print(return_id)
+            # print(return_password)
+
+            decoded_password = decoder.EncryptPasswords(return_password).decryptPassword()
+
+            if decoded_password == password:
+                return render(request, "homepage/DoctorHome.html")
+            else:
+                return HttpResponse("Wrong Pass")
         else:
             print("NOT FOUND")  # TODO: DEBUG NOT PRINTING
 
