7e4e8a6d3afc60a75b76fc48d8fd9c4e855b2d8f
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-10-31 19:17:49 +0600

    Log In complete for all users

--- LogIn/views.py
+++ LogIn/views.py
@@ -50,12 +50,64 @@ def submit(request):
             else:
                 return HttpResponse("Wrong Pass")
         else:
-            print("NOT FOUND")  # TODO: DEBUG NOT PRINTING
+            return HttpResponse("Database Error or You don't exist")
 
     elif user == "User":
-        return render(request, "homepage/UserHome.html")
+        statement = "SELECT USER_ID, PASSWORD from MEDI_SHEBA.USERS WHERE EMAIL=" + "\'" + email + "\'"
+        c.execute(statement)
+        if c:
+            x = c.fetchone()
+            return_id = x[0]
+            return_password = x[1]
+            # print(return_id)
+            # print(return_password)
+
+            decoded_password = decoder.EncryptPasswords(return_password).decryptPassword()
+
+            if decoded_password == password:
+                return render(request, "homepage/UserHome2.html")
+            else:
+                return HttpResponse("Wrong Pass")
+        else:
+            return HttpResponse("Database Error or You don't exist")
+
     elif user == "HospitalAdmin":
-        return render(request, "homepage/HospitalAdminHome.html")
+        statement = "SELECT HOSPITAL_ID,PASSWORD from MEDI_SHEBA.HOSPITAL WHERE EMAIL=" + "\'" + email + "\'"
+        c.execute(statement)
+        if c:
+            x = c.fetchone()
+            return_id = x[0]
+            return_password = x[1]
+            # print(return_id)
+            # print(return_password)
+
+            decoded_password = decoder.EncryptPasswords(return_password).decryptPassword()
+
+            if decoded_password == password:
+                return render(request, "homepage/HospitalAdminHome.html")
+            else:
+                return HttpResponse("Wrong Pass")
+        else:
+            return HttpResponse("Database Error or You don't exist")
+
+    elif user == "BloodBankAdmin":
+        statement = "SELECT BLOOD_BANK_ID,PASSWORD from MEDI_SHEBA.BLOOD_BANK WHERE EMAIL=" + "\'" + email + "\'"
+        c.execute(statement)
+        if c:
+            x = c.fetchone()
+            return_id = x[0]
+            return_password = x[1]
+            # print(return_id)
+            # print(return_password)
+
+            decoded_password = decoder.EncryptPasswords(return_password).decryptPassword()
+
+            if decoded_password == password:
+                return render(request, "homepage/BloodbankHome2.html")
+            else:
+                return HttpResponse("Wrong Pass")
+        else:
+            return HttpResponse("Database Error or You don't exist")
     return render(request, "auth/LogInOrSignUp.html")
 
 
